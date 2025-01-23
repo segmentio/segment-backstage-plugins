241f808dfba1a1c411d9761d2cf914c44a90011e
Alec Jacobs <charles.jacobs@segment.com>
2025-01-23 10:21:10 -0800

    feat: backstage v1.35 package bump (#21)
    Signed-off-by: Alec Jacobs <charles.jacobs@segment.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,31 +3,31 @@ import { createBackend } from '@backstage/backend-defaults';
 const backend = createBackend();
 
 // app plugin installation
-backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(import('@backstage/plugin-app-backend'));
 
 // proxy plugin installation
-backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+backend.add(import('@backstage/plugin-proxy-backend'));
 
 // techdocs plugin installation
-backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
+backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // catalog plugin installation
-backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
 // scaffolder plugin installation
-backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend'));
 
 // auth plugin installation
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
 // search plugin installation
-backend.add(import('@backstage/plugin-search-backend/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+backend.add(import('@backstage/plugin-search-backend'));
+backend.add(import('@backstage/plugin-search-backend-module-catalog'));
+backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // proxy-sigv4 plugin installation
 backend.add(import('@segment/backstage-plugin-proxy-sigv4-backend'));
